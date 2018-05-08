80afc95ec7e0bc5f9ec8c62f040838d3c5873fbf
MarkHoward <markjameshoward@hotmail.co.uk>
2018-05-08 12:28:52 +1200

    Enable customize

--- CharacterDesign.js
+++ CharacterDesign.js
@@ -13,49 +13,27 @@ export class CharacterDesign extends LitElement {
     this.allowMouthMoveRight = true;
     this.allowMoveEyesLeft = true;
     this.allowMoveEyesRight = true;
-    // this.level = 5;
     this.eyes = 1;
-    this.mouth = 1;
+    this.mouth = 4;
+    this.silhouette = 1;
     this.NUMBER_OF_IMAGES_EYES = 6;
     this.NUMBER_OF_IMAGES_MOUTH = 9;
     this.NUMBER_OF_IMAGES_SILHOUETTES = 4;
     this.locks = [];
+    this.level = 1;
   }
 
-  ApplyLockImagesToSilhouettes() {
-
-    for (var i = 1; i < this.NUMBER_OF_IMAGES_SILHOUETTES; i++) {
-      this.locks[i] = false;
-    }
-
-    for (var i = this.level; i >= 1; i--) {
-     this.locks[i] = true;
-    }
-
-    // for (var i = this.level; i >= 1; i--) {
-    //   let q = `lock${i}`;
-    //   let sil = this.$[q];
-    //   if (sil != undefined) {
-    //     sil.classList.add("NoPadlock");
-    //   }
-    // }
-  }
-
-  _levelChanged(newValue, oldValue) {
-    console.log('level changed')
-    this.ConfigureButtons();
-  }
 
   static get properties() {
     return {
       silhouette: Number,
-      level: { Type: Number, value: 1, observer: '_levelChanged' },
+      level: { Type: Number, value: 1 },
       mouth: Number,
       eyes: Number,
       NUMBER_OF_IMAGES_EYES: Number,
       NUMBER_OF_IMAGES_MOUTH: Number,
       NUMBER_OF_IMAGES_SILHOUETTES: Number,
-      customize: { Type: Boolean, value: true },
+      enableCustomize: Boolean,
       allowMoveEyesRight: Boolean,
       allowMoveEyesLeft: Boolean,
       showMouthMoveLeft: Boolean,
@@ -70,31 +48,39 @@ export class CharacterDesign extends LitElement {
     };
   }
 
-  ConfigureButtons() {
-
-    if (this.eyes >= this.NUMBER_OF_IMAGES_EYES || this.eyes >= this.level) {
-      this.allowMoveEyesRight = false;
-    } else {
-      this.allowMoveEyesRight = true;
-    }
-
-    if (this.eyes <= 1) {
-      this.allowMoveEyesLeft = false;
-    } else {
-      this.allowMoveEyesLeft = true;
-    }
-
-    if (this.mouth <= 1) {
-      this.allowMouthMoveLeft = false;
-    } else {
-      this.allowMouthMoveLeft = true;
-    }
-
+  AllowMouthMoveRight() {
     if (this.mouth >= this.NUMBER_OF_IMAGES_MOUTH || this.mouth >= this.level) {
-      this.allowMouthMoveRight = false;
+      return false;
     } else {
-      this.allowMouthMoveRight = true;
+      return true;
     }
+  }
+
+  AllowMouthMoveLeft() {
+    if (this.mouth <= 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  AllowEyesMoveRight() {
+    if (this.eyes >= this.NUMBER_OF_IMAGES_EYES || this.eyes >= this.level) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  AllowEyesMoveLeft() {
+    if (this.eyes <= 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  ConfigureButtons() {
 
     if (this.customize === "false") {
       this.showMouthMoveLeft = false;
@@ -107,19 +93,12 @@ export class CharacterDesign extends LitElement {
       // this.$.ArrowMouthRight.classList.add("DisableCustomize");
       // this.$.SilhouetteSelector.classList.add("DisableCustomize");
     }
-
-     this.ApplyLockImagesToSilhouettes();
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.ConfigureButtons();
   }
 
   _render({ silhouette, level, mouth, eyes, NUMBER_OF_IMAGES_EYES
-    , NUMBER_OF_IMAGES_MOUTH, NUMBER_OF_IMAGES_SILHOUETTES, customize,
+    , NUMBER_OF_IMAGES_MOUTH, NUMBER_OF_IMAGES_SILHOUETTES, enableCustomize,
     allowMouthMoveLeft, allowMouthMoveRight, allowMoveEyesLeft, allowMoveEyesRight,
-  locks }) {
+    locks }) {
     return html`
     <style>
       :root {
@@ -252,66 +231,80 @@ export class CharacterDesign extends LitElement {
         background-size: contain;
       }
     </style>
-    
     <div class="CharacterCustomizeMain">
     
       <div class='SilhouetteBackgroundContainer'>
         <img width=80% style='display: block; margin: 0 auto' src='./images/silhouette/silhouette${silhouette}.png'>
       </div>
     
+    
       <div class='eyesselector'>
         <div class='eyes'>
     
-          <div class$="${allowMoveEyesLeft ? 'LeftArrow' : 'LeftArrow ArrowDisabled'}" on-click='${() => this.moveEyesLeft()}'>
+          <div class$="${this.enableCustomize ? '' : 'DisableCustomize'}">
+            <div class$="${this.AllowEyesMoveLeft() ? 'LeftArrow' : 'LeftArrow ArrowDisabled'}" on-click='${() => this.moveEyesLeft()}'>
+            </div>
           </div>
     
           <div style="width:30%">
             <img width="100%" src="./images/eyes/eyes${eyes}.png">
           </div>
     
-          <div class$="${allowMoveEyesRight ? 'RightArrow' : 'RightArrow ArrowDisabled'}" on-click='${() => this.moveEyesRight()}'>
+          <div class$="${this.enableCustomize ? '' : 'DisableCustomize'}">
+            <div class$="${this.AllowEyesMoveRight() ? 'RightArrow' : 'RightArrow ArrowDisabled'}" on-click='${() => this.moveEyesRight()}'>
+            </div>
           </div>
     
         </div>
       </div>
     
     
-      <div class="mouthselector">
-        <div class='mouth'>
     
-          <div class$="${allowMouthMoveLeft ? 'LeftArrow' : 'LeftArrow ArrowDisabled'}" on-click="${() => this.moveMouthLeft()}"></div>
     
-          <div style="width:30%">
-            <img id="mouth" style="width:100%;" src="./images/mouths/mouth${mouth}.png">
-          </div>
+    <div class="mouthselector">
+      <div class='mouth'>
     
-          <div  class$="${allowMouthMoveRight ? 'RightArrow' : 'RightArrow ArrowDisabled'}" on-click="${() => this.moveMouthRight()}">
-          </div>
-    
+        <div class$="${this.enableCustomize ? '' : 'DisableCustomize'}">
+          <div class$="${this.AllowMouthMoveLeft() ? 'LeftArrow' : 'LeftArrow ArrowDisabled'}" on-click="${() => this.moveMouthLeft()}"></div>
         </div>
+    
+        <div style="width:30%">
+          <img id="mouth" style="width:100%;" src="./images/mouths/mouth${mouth}.png">
+        </div>
+    
+        <div class$="${this.enableCustomize ? '' : 'DisableCustomize'}">
+          <div class$="${this.AllowMouthMoveRight() ? 'RightArrow' : 'RightArrow ArrowDisabled'}" on-click="${() => this.moveMouthRight()}">
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
+    <div id='SilhouetteSelector' class$="${this.enableCustomize ? 'silhouette' : 'DisableCustomize'}">
+    
+      <div on-click="${() => this.Pick(1)}" class="OverlayTwoItemsCharacter silhouettePicker">
+        <img style="grid-area:main;width:100%" class="sil1" src="./images/silhouette/silhouette1.png">
+        <iron-icon class$="${this.level >= 1 ? 'NoPadlock' : ''}" style="grid-area:main;z-index:2;align-self:center;justify-self:center"
+          icon="lock"></iron-icon>
       </div>
     
-      <div id='SilhouetteSelector' class='silhouette'>
-    
-        <div on-click="${() => this.Pick(1)}" class="OverlayTwoItemsCharacter silhouettePicker">
-          <img style="grid-area:main;width:100%" class="sil1" src="./images/silhouette/silhouette1.png">
-          <iron-icon class$="${locks[1] ? 'NoPadlock' : '' }" style="grid-area:main;z-index:2;align-self:center;justify-self:center" icon="lock"></iron-icon>
-        </div>
-    
-        <div on-click="${() => this.Pick(2)}" class="OverlayTwoItemsCharacter silhouettePicker">
-          <iron-icon class$="${locks[2] ? 'NoPadlock' : '' }" style="grid-area:main;z-index:2;align-self:center;justify-self:center" icon="lock"></iron-icon>
-          <img style="grid-area:main;width:100%" class="sil2" src="./images/silhouette/silhouette2.png">
-        </div>
-    
-        <div on-click="${() => this.Pick(3)}" class="OverlayTwoItemsCharacter silhouettePicker">
-          <img style="grid-area:main;width:100%" class="sil3" src="./images/silhouette/silhouette3.png">
-          <iron-icon class$="${locks[3] ? 'NoPadlock' : '' }" style="grid-area:main;z-index:2;align-self:center;justify-self:center" icon="lock"></iron-icon>
-        </div>
-        <div on-click="${() => this.Pick(4)}" class="OverlayTwoItemsCharacter silhouettePicker">
-          <img style="grid-area:main;width:100%" class="sil4" on-click='PickSilhouetee(4)' src="./images/silhouette/silhouette4.png">
-          <iron-icon class$="${locks[4] ? 'NoPadlock' : '' }" style="grid-area:main;z-index:2;align-self:center;justify-self:center" icon="lock"></iron-icon>
-        </div>
+      <div on-click="${() => this.Pick(2)}" class="OverlayTwoItemsCharacter silhouettePicker">
+        <iron-icon class$="${this.level >= 2 ? 'NoPadlock' : ''}" style="grid-area:main;z-index:2;align-self:center;justify-self:center"
+          icon="lock"></iron-icon>
+        <img style="grid-area:main;width:100%" class="sil2" src="./images/silhouette/silhouette2.png">
       </div>
+    
+      <div on-click="${() => this.Pick(3)}" class="OverlayTwoItemsCharacter silhouettePicker">
+        <img style="grid-area:main;width:100%" class="sil3" src="./images/silhouette/silhouette3.png">
+        <iron-icon class$="${this.level >= 3 ? 'NoPadlock' : ''}" style="grid-area:main;z-index:2;align-self:center;justify-self:center"
+          icon="lock"></iron-icon>
+      </div>
+      <div on-click="${() => this.Pick(4)}" class="OverlayTwoItemsCharacter silhouettePicker">
+        <img style="grid-area:main;width:100%" class="sil4" on-click='PickSilhouetee(4)' src="./images/silhouette/silhouette4.png">
+        <iron-icon class$="${this.level >= 4 ? 'NoPadlock' : ''}" style="grid-area:main;z-index:2;align-self:center;justify-self:center"
+          icon="lock"></iron-icon>
+      </div>
+    </div>
     
     </div>
         `
